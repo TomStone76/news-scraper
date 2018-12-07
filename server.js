0c77b3a2924fb9064262e7685265d10205be707c
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-07 13:51:49 -0500

    server updates

--- server.js
+++ server.js
@@ -20,4 +20,47 @@ app.get("/scrape", function(req, res) {
     axios.get("http://www.bbc.com/news").then(function(response) {
         let $ = cheerio.load(response.data);
         $("article h2").each(function(i, element) {
+            let result = {};
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
             
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+            });
+
+            res.send("Scrape Complete");
+        });
+    });
+
+    app.get("/articles", function(req, res) {
+        db.Article.find({})
+            .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+    });
+
+    app.get("/articles/:id", function(req, res) {
+        db.Article.findOne({ _id: req.params.id })
+            .populate("note")
+            .then(function(dbArticle) { 
+                res.json(dbArticle);
+            })
+            .catch(function(err) {
+                res.json(err);
+            });
+    });
+
+
+    
