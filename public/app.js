6787870699c385e59c3a4a7eed3fb67ba3d2e5ae
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-07 14:14:49 -0500

    finish app.js draft

--- public/app.js
+++ public/app.js
@@ -1,47 +1,47 @@
-$.getJSON("/articles", function(data) {
+$.getJSON("/articles", function (data) {
     for (let i = 0; i < data.length; i++) {
         $("#articles").append("<p data-id='" + data[i].id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
     }
 });
 
-$(document).on("click", "p", function() {
+$(document).on("click", "p", function () {
     $("#notes").empty();
     let thisId = $(this).attr("data-id");
 
     $.ajax({
-        method: "GET",
-        url: "/articles/" + thisId
-    })
+            method: "GET",
+            url: "/articles/" + thisId
+        })
 
-    .then(function(data) {
-        console.log(data);
-        $("#notes").append("<h2" + data.title + "<h2>");
-        $("#notes").append("<input id='titleinput' name='title' >");
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        $("#notes").append("button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+        .then(function (data) {
+            console.log(data);
+            $("#notes").append("<h2" + data.title + "<h2>");
+            $("#notes").append("<input id='titleinput' name='title' >");
+            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+            $("#notes").append("button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
-        if (data.note) {
-            $("#titleinput").val(data.note.title);
-            $("#bodyinput").val(data.note.body);
-        }
-    });
+            if (data.note) {
+                $("#titleinput").val(data.note.title);
+                $("#bodyinput").val(data.note.body);
+            }
+        });
 });
 
-$(document).on("click", "#savenote", function() {
+$(document).on("click", "#savenote", function () {
     let thisId = $(this).attr("data-id");
 
     $.ajax({
-        method: "POST",
-        url: "/articles/" + thisId,
-        data: {
-            title: $("#titleinput").val(),
-            body: $("#bodyinput").val()
-        }
-    })
-    .then(function(data) {
-        console.log(data);
-        $("#notes").empty();
-    });
+            method: "POST",
+            url: "/articles/" + thisId,
+            data: {
+                title: $("#titleinput").val(),
+                body: $("#bodyinput").val()
+            }
+        })
+        .then(function (data) {
+            console.log(data);
+            $("#notes").empty();
+        });
 
     $("#titlinput").val("");
     $("#bodyinput").val("");
