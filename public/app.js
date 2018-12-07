4186499fe9e6c0b395074f0c3ced4a36986f3431
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-07 14:05:26 -0500

    app.js logic modifications

--- public/app.js
+++ public/app.js
@@ -1,5 +1,23 @@
 $.getJSON("/articles", function(data) {
     for (let i = 0; i < data.length; i++) {
-        $("#articles").append("<p data-id='" + data[i].id + "'>" + data[i].title + "<br />" + data[i].link) + "</p>");
+        $("#articles").append("<p data-id='" + data[i].id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
     }
 });
+
+$(document).on("click", "p", function() {
+    $("#notes").empty();
+    let thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+
+    .then(function(data) {
+        console.log(data);
+        $("#notes").append("<h2" + data.title + "<h2>");
+        $("#notes").append("<input id='titleinput' name='title' >");
+        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+        $("#notes").append("button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+        if (data.note)
